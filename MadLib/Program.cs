626fdac1b7b0dffba72af8a81ce111e77f7a694c
Yun Jang <jang.yunj7@gmail.com>
2023-01-19 12:57:59 -0500

    initial commit

---
+++ MadLib/Program.cs
@@ -0,0 +1,18 @@
+﻿string color;
+string pluralNoun;
+string celebrity;
+
+Console.Write("Enter a color: ");
+color = Console.ReadLine();
+
+Console.Write("Enter a pluralNoun: ");
+pluralNoun = Console.ReadLine();
+
+Console.Write("Enter a celebrity: ");
+celebrity = Console.ReadLine();
+
+Console.WriteLine($"Roses are {color}.");
+Console.WriteLine($"{pluralNoun} are blue.");
+Console.WriteLine($"I love {celebrity}.");
+
+Console.ReadLine();
